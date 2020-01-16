b7cebea5f7e2cd875ee2f326c138bd097f96d157
zyf <zyf@163.com>
2020-01-16 15:57:48 +0800

    小龙女第二次提交

--- src/com/woniuxy/servlets/LifeServlet.java
+++ src/com/woniuxy/servlets/LifeServlet.java
@@ -14,6 +14,6 @@ public class LifeServlet extends HttpServlet{
 	   @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("hahahahahahhah");
+		System.out.println("xiaolongnv");
 	}
 }
