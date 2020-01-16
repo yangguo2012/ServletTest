8d8d4a518c75cbd5db6b786a94d44d032fac3865
zyf <zyf@163.com>
2020-01-16 15:45:08 +0800

    小龙女提交第一次

--- src/com/woniuxy/servlets/LifeServlet.java
+++ src/com/woniuxy/servlets/LifeServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class LifeServlet extends HttpServlet{
-   
-
+     
+	   @Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 }
